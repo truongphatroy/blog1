fc4112c6401dfc8960626eaf7e6e09bad7927450
truong <truongphatroy@gmail.com>
2023-05-26 07:29:01 +0900

    config morgan OK

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const { engine } = require("express-handlebars");
+const app = express();
+const port = 3000;
+
+// console.log(app);
+
+// HTTP logged
+app.use(morgan("combined"));
+
+// Template engine
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+// app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "resources/views"));
+// console.log(path.join(__dirname, "resources/views"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/tintuc", (req, res) => {
+  res.render("tintuc");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
